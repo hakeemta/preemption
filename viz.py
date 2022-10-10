af8ff8537f2b61ddf705aa2a707526e7d6223441
Abdulhakeem Abdulrahman <nicemate17@gmail.com>
2022-10-10 12:30:50 +0100

    Combines contigous blocks

--- viz.py
+++ viz.py
@@ -1,3 +1,4 @@
+from turtle import width
 import numpy as np
 import pandas as pd
 
@@ -33,42 +34,43 @@ def plot_task_events(i, task, H, for_deadlines=False):
     return scatter
 
 
-def plot_trace(_id, traces, H):
-    traces = np.clip(traces, a_min=-1, a_max=100)
-    trace = np.where(traces == (_id + 1)) [0]
+def plot_trace(_id, df_traces, H):
+    df_trace = df_traces[ df_traces['id'] == str(_id + 1) ]
+    trace = df_trace.start
 
     theta_per_tick = PERIOD / H
-    r = 40 - 30 * (trace // H)
-    theta = ((trace + 0.5) % H) * theta_per_tick
+    width = theta_per_tick * df_trace['dt']
+    r = 50 - 5 * (trace // H)
+    theta = (trace + 0.5 * df_trace['dt']) * theta_per_tick
 
-    bar = go.Barpolar(r=r, theta=theta, width=theta_per_tick, name=f'Task {_id + 1}',
-                    marker=dict(color=COLORS[_id], line_width=0.2, line_color='white'), 
+    bar = go.Barpolar(r=r, base=0, theta=theta, width=width, name=f'Task {_id + 1}',
+                    marker=dict(color=COLORS[_id], line_width=1, line_color='black'), 
                     opacity=0.8)
 
     return bar
 
 
-def plot_polar(tasks, traces):
+def plot_polar(tasks, df_traces, size=800):
     fig_polar = go.Figure()
 
     H = compute_H(tasks)
     ticks = np.arange(H)
     for i, task in enumerate(tasks):
         scatter_releases = plot_task_events(i, task, H)
-        scatter_deadlines = plot_task_events(i, task, H, for_deadlines=True)
-
-        bar = plot_trace(i, traces, H)
-
         fig_polar.add_trace(scatter_releases)
+
+        scatter_deadlines = plot_task_events(i, task, H, for_deadlines=True)
         fig_polar.add_trace(scatter_deadlines)
-        fig_polar.add_trace(bar)
+
+        bar = plot_trace(i, df_traces, H)
+        fig_polar.add_trace(bar)    
 
     # Add cost trace
-    bar = plot_trace(-2, traces, H)
+    bar = plot_trace(-1, df_traces, H)
     bar.showlegend = False
     fig_polar.add_trace(bar)
 
-    fig_polar.update_layout(template=None, autosize=False, width=800, height=800,
+    fig_polar.update_layout(template=None, autosize=False, width=size, height=size,
                             polar=dict(angularaxis=dict(ticks='outside',
                                                 # type='category', period=H,
                                                 tickmode='array', tickvals=ticks * (360 / H), ticktext=ticks),
@@ -77,7 +79,7 @@ def plot_polar(tasks, traces):
     fig_polar.show()
 
 
-def plot_gantt(tasks, traces):
+def process_traces(traces):
     df_traces = pd.DataFrame(traces, columns=['id'])
 
     df_traces['dt'] = df_traces['id'].ne( df_traces['id'].shift() ).cumsum()
@@ -91,9 +93,13 @@ def plot_gantt(tasks, traces):
     df_traces.loc[ df_traces['id'] < 0, 'id'] = 0
     df_traces[['id']] = df_traces[['id']].astype(str)
 
+    return df_traces
+
+
+def plot_gantt(tasks, df_traces, fig_size=None):
     fig_gantt = px.timeline(df_traces, x_start='start', x_end='end', y='task', 
                         color='id', color_discrete_sequence=COLORS, opacity=0.8)
-    fig_gantt.update_traces(marker=dict(line_color='black', line_width=1))
+    fig_gantt.update_traces(marker=dict(line_color='black', line_width=1.0))
         
     y_offset = -0.25
     n = len(tasks)
@@ -128,9 +134,8 @@ def plot_gantt(tasks, traces):
 
         i = int(data.name) - 1
         data.marker.color = COLORS[i]
-        
+
         if i == -1:
-            data.marker.color = COLORS[i-1]
             data.showlegend = False
             continue
 
@@ -139,9 +144,11 @@ def plot_gantt(tasks, traces):
         
         # Add events
         H = compute_H(tasks)
-        plot_task_events_gantt(i, tasks[i], H, size=traces.size)
-        plot_task_events_gantt(i, tasks[i], H, for_deadlines=True, size=traces.size)
+        size = df_traces.shape[0]
+        plot_task_events_gantt(i, tasks[i], H, size=size)
+        plot_task_events_gantt(i, tasks[i], H, for_deadlines=True, size=size)
 
+    fig_gantt.update_layout(width=fig_size, height=fig_size)
     fig_gantt.update_xaxes(type='linear')
     fig_gantt.update_yaxes(type='category', categoryorder='category descending')
     fig_gantt.show()
